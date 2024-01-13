82cbcb091ba4798d1f3246f454ca93b581a49c9e
edgarmuyomba <edgarmatthew247@gmail.com>
2024-01-13 21:32:53 +0300

    dashboard arrangement

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,7 +3,24 @@ import styles from "./styles.module.css";
 export default function Dashboard() {
     return (
         <div className={styles.dashboard}>
-            Dashboard
+            <div className={styles.users}>
+                Users
+            </div>
+            <div className={styles.sensors}>
+                Sensors
+            </div>
+            <div className={styles.income}>
+                Income
+            </div>
+            <div className={styles.occupancy}>
+                Occupancy 
+            </div>
+            <div className={styles.session_tracking}>
+                Session Tracking 
+            </div>
+            <div className={styles.parking_lots}>
+                Parking Lots
+            </div>
         </div>
     );
 }
