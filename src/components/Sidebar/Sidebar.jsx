4cfc9fa11c52a12146c697a016695d3d10a9fd8a
edgarmuyomba <edgarmatthew247@gmail.com>
2024-01-13 13:17:54 +0300

    sidebar styles done

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,49 @@
+import styles from "./styles.module.css";
+import { Link } from "react-router-dom";
+import Icon from "@mdi/react";
+import { mdiViewDashboard, mdiParking, mdiAvTimer, mdiAccountSettings, mdiSmokeDetector, mdiChartBox } from "@mdi/js";
+
 export default function Sidebar() {
     return (
-        <></>
+        <aside className={styles.sidebar}>
+            <ul className={styles.links}>
+                <Link id={styles.active}>
+                    <li className={styles.link}>
+                        <Icon path={mdiViewDashboard} size={1} color="rgb(241, 241, 241)" />
+                        <p className={styles.name}>Dashboard</p>
+                    </li>
+                </Link>
+                <Link id={styles.tab}>
+                    <li className={styles.link}>
+                        <Icon path={mdiParking} size={1} color="rgb(241, 241, 241)" />
+                        <p className={styles.name}>Parking Lots</p>
+                    </li>
+                </Link>
+                <Link id={styles.tab}>
+                    <li className={styles.link}>
+                        <Icon path={mdiAvTimer} size={1} color="rgb(241, 241, 241)" />
+                        <p className={styles.name}>Sessions</p>
+                    </li>
+                </Link>
+                <Link id={styles.tab}>
+                    <li className={styles.link}>
+                        <Icon path={mdiAccountSettings} size={1} color="rgb(241, 241, 241)" />
+                        <p className={styles.name}>Users</p>
+                    </li>
+                </Link>
+                <Link id={styles.tab}>
+                    <li className={styles.link}>
+                        <Icon path={mdiSmokeDetector} size={1} color="rgb(241, 241, 241)" />
+                        <p className={styles.name}>Sensors</p>
+                    </li>
+                </Link>
+                <Link id={styles.tab}>
+                    <li className={styles.link}>
+                        <Icon path={mdiChartBox} size={1} color="rgb(241, 241, 241)" />
+                        <p className={styles.name}>Reports</p>
+                    </li>
+                </Link>
+            </ul>
+        </aside>
     )
 }
