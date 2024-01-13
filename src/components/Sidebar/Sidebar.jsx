aeebfa2501cbdb1878c4a019e8c8505bfdc9f5f5
edgarmuyomba <edgarmatthew247@gmail.com>
2024-01-13 21:05:17 +0300

    sidebar navigation working

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,43 +1,47 @@
 import styles from "./styles.module.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
 import Icon from "@mdi/react";
 import { mdiViewDashboard, mdiParking, mdiAvTimer, mdiAccountSettings, mdiSmokeDetector, mdiChartBox } from "@mdi/js";
 
 export default function Sidebar() {
+    const [active, setActive] = useState(1);
+
     return (
         <aside className={styles.sidebar}>
             <ul className={styles.links}>
-                <Link id={styles.active}>
+                <Link id={active == 1 ? styles.active : styles.tab} onClick={() => setActive(1)} to="/">
                     <li className={styles.link}>
                         <Icon path={mdiViewDashboard} size={1} color="rgb(241, 241, 241)" />
                         <p className={styles.name}>Dashboard</p>
                     </li>
                 </Link>
-                <Link id={styles.tab}>
+                <Link id={active == 2 ? styles.active : styles.tab} onClick={() => setActive(2)} to="parking_lots">
                     <li className={styles.link}>
                         <Icon path={mdiParking} size={1} color="rgb(241, 241, 241)" />
                         <p className={styles.name}>Parking Lots</p>
                     </li>
                 </Link>
-                <Link id={styles.tab}>
+                <Link id={active == 3 ? styles.active : styles.tab} onClick={() => setActive(3)} to="sessions">
                     <li className={styles.link}>
                         <Icon path={mdiAvTimer} size={1} color="rgb(241, 241, 241)" />
                         <p className={styles.name}>Sessions</p>
                     </li>
                 </Link>
-                <Link id={styles.tab}>
+                <Link id={active == 4 ? styles.active : styles.tab} onClick={() => setActive(4)} to="users">
                     <li className={styles.link}>
                         <Icon path={mdiAccountSettings} size={1} color="rgb(241, 241, 241)" />
                         <p className={styles.name}>Users</p>
                     </li>
                 </Link>
-                <Link id={styles.tab}>
+                <Link id={active == 5 ? styles.active : styles.tab} onClick={() => setActive(5)} to="sensors">
                     <li className={styles.link}>
                         <Icon path={mdiSmokeDetector} size={1} color="rgb(241, 241, 241)" />
                         <p className={styles.name}>Sensors</p>
                     </li>
                 </Link>
-                <Link id={styles.tab}>
+                <Link id={active == 6 ? styles.active : styles.tab} onClick={() => setActive(6)} to="reports">
                     <li className={styles.link}>
                         <Icon path={mdiChartBox} size={1} color="rgb(241, 241, 241)" />
                         <p className={styles.name}>Reports</p>
